8ea12022882b4c06973cde6ce4c9f8558aeee5bd
yichaoS <yichao@cbio.mskcc.org>
2016-02-02 17:30:39 -0500

    added fixed header; added view type in json data;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,10 @@ var iViz = (function() {
       width: 150,
       height: 150,
       inner_radius: 15
+    },
+    bar_chart: {
+      width: 400,
+      height: 180
     }
   };
     return {
@@ -32,42 +36,125 @@ var iViz = (function() {
             });
 
             // ---- define charts ----
+
+            //// ---- patient charts ----
+
             _.each(data.groups.patient.attr_meta, function(_attr_obj) {
-              var _attr_id = _attr_obj.attr_id;
-              var _dim = ndx_patient.dimension(function(d) { return d[_attr_id]; });
-              var _countPerFunc = _dim.group().reduceCount();
-              $("#main-grid").append(
-                "<div class='grid-item'>" +
-                "<p class='text-center'>" + _attr_obj.display_name + "</p>" +
-                "<div class='dc-chart dc-bar-chart' id='chart-ring-patient-" + _attr_id + "'></div></div>");
-              var _chart = dc.pieChart("#chart-ring-patient-" + _attr_id);
-              _chart.width(settings.pie_chart.width)
-                .height(settings.pie_chart.height)
-                .dimension(_dim)
-                .group(_countPerFunc)
-                .innerRadius(settings.pie_chart.inner_radius);
-            });
-            _.each(data.groups.sample.attr_meta, function(_attr_obj) {
-              var _attr_id = _attr_obj.attr_id;
-              if (_attr_id !== "mutated_genes" && _attr_id !== "cna_details") {
-                var _dim = ndx_sample.dimension(function(d) { return d[_attr_id]; });
+
+              if (_attr_obj.view_type === "pie_chart") {
+
+                var _attr_id = _attr_obj.attr_id;
+                var _dim = ndx_patient.dimension(function(d) { return d[_attr_id]; });
                 var _countPerFunc = _dim.group().reduceCount();
+
                 $("#main-grid").append(
                   "<div class='grid-item'>" +
                   "<p class='text-center'>" + _attr_obj.display_name + "</p>" +
-                  "<div class='dc-chart dc-bar-chart' id='chart-ring-sample-" + _attr_id + "'></div></div>");
-                var _chart = dc.pieChart("#chart-ring-sample-" + _attr_id);
+                  "<div class='dc-chart dc-bar-chart' id='chart-ring-patient-" + _attr_id + "'></div></div>");
+
+                var _chart = dc.pieChart("#chart-ring-patient-" + _attr_id);
+
                 _chart.width(settings.pie_chart.width)
                   .height(settings.pie_chart.height)
                   .dimension(_dim)
                   .group(_countPerFunc)
                   .innerRadius(settings.pie_chart.inner_radius);
+
+              } else if (_attr_obj.view_type === "bar_chart") {
+
+                var _attr_id = _attr_obj.attr_id;
+                var _dim = ndx_patient.dimension(function(d) { return d[_attr_id]; });
+                var _countPerFunc = _dim.group().reduceCount();
+
+                var _bar_chart_data = _.map(_.filter(_.pluck(patient_data, _attr_id), function(d) { return d !== "NA"}), function(d) { return parseFloat(d); });
+
+                $("#main-grid").append(
+                  "<div class='grid-item grid-item--width2'>" +
+                  "<div class='dc-chart dc-bar-chart' id='chart-bar-patient-" + _attr_id + "'></div></div>");
+
+                  var _chart = dc.barChart("#chart-bar-patient-" + _attr_id);
+
+                  _chart.width(400)
+                    .height(180)
+                    .gap(2)
+                    .dimension(_dim)
+                    .group(_countPerFunc)
+                    .x(d3.scale.linear().domain([d3.min(_bar_chart_data), d3.max(_bar_chart_data)]))
+                    .elasticY(true)
+                    .centerBar(true)
+                    .xAxisLabel(_attr_obj.display_name)
+                    .margins({top: 10, right: 20, bottom: 50, left: 50});
+
+              } else if (_attr_obj.view_type === "scatter_plots") {
+                //TODO:
+              } else if (_attr_obj.view_type === "table") {
+                //TODO:
+              } else {
+                //TODO:
               }
+
+
+            });
+            _.each(data.groups.sample.attr_meta, function(_attr_obj) {
+
+              if (_attr_obj.view_type === "pie_chart") {
+
+                var _attr_id = _attr_obj.attr_id;
+                var _dim = ndx_sample.dimension(function(d) { return d[_attr_id]; });
+                var _countPerFunc = _dim.group().reduceCount();
+
+                $("#main-grid").append(
+                  "<div class='grid-item'>" +
+                  "<p class='text-center'>" + _attr_obj.display_name + "</p>" +
+                  "<div class='dc-chart dc-bar-chart' id='chart-ring-sample-" + _attr_id + "'></div></div>");
+
+                var _chart = dc.pieChart("#chart-ring-sample-" + _attr_id);
+
+                _chart.width(settings.pie_chart.width)
+                  .height(settings.pie_chart.height)
+                  .dimension(_dim)
+                  .group(_countPerFunc)
+                  .innerRadius(settings.pie_chart.inner_radius);
+
+              } else if (_attr_obj.view_type === "bar_chart") {
+
+                var _attr_id = _attr_obj.attr_id;
+                var _dim = ndx_sample.dimension(function(d) { return d[_attr_id]; });
+                var _countPerFunc = _dim.group().reduceCount();
+
+                var _bar_chart_data = _.map(_.filter(_.pluck(sample_data, _attr_id), function(d) { return d !== "NA"}), function(d) { return parseFloat(d); });
+
+                $("#main-grid").append(
+                  "<div class='grid-item grid-item--width2'>" +
+                  "<div class='dc-chart dc-bar-chart' id='chart-bar-sample-" + _attr_id + "'></div></div>");
+
+                var _chart = dc.barChart("#chart-bar-sample-" + _attr_id);
+
+                _chart.width(settings.bar_chart.width)
+                  .height(settings.bar_chart.height)
+                  .dimension(_dim)
+                  .group(_countPerFunc)
+                  .x(d3.scale.linear().domain([d3.min(_bar_chart_data) - (d3.max(_bar_chart_data) - d3.min(_bar_chart_data)) * 0.1, d3.max(_bar_chart_data)]))
+                  .elasticY(true)
+                  .centerBar(true)
+                  .xAxisLabel(_attr_obj.display_name)
+                  .margins({top: 10, right: 20, bottom: 50, left: 50});
+
+              } else if (_attr_obj.view_type === "scatter_plots") {
+                //TODO:
+              } else if (_attr_obj.view_type === "table") {
+                //TODO:
+              } else {
+                //TODO:
+              }
+
             });
 
             // ---- render ----
             var $grid = $('.grid').packery({
-              itemSelector: '.grid-item'
+              itemSelector: '.grid-item',
+              columnWidth: 250,
+              rowHeight: 250
             });
             $grid.find('.grid-item').each(function(i, gridItem) {
               var draggie = new Draggabilly(gridItem);
