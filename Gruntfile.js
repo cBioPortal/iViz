865ca84a6c1974beadea4b600119d4cc42b5c02c
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-03-29 18:23:36 -0400

    Change configuration files to fix the grunt build issues.

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,6 +359,12 @@ module.exports = function(grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass/assets/fonts/bootstrap/*',
           dest: '<%= config.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: '.',
+          src: 'bower_components/components-font-awesome/fonts/*',
+          dest: '<%= config.dist %>'
         }]
       }
     },
