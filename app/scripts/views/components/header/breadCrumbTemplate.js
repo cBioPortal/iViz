9c7586a6d5b7e5b5ccce111cb7ed7488e67e0471
yichaoS <yichao@cbio.mskcc.org>
2016-06-09 13:03:32 -0400

    makeover for breadcrumb

--- app/scripts/views/components/header/breadCrumbTemplate.js
+++ app/scripts/views/components/header/breadCrumbTemplate.js
@@ -35,15 +35,15 @@
 'use strict';
 (function(Vue) {
   Vue.component('breadCrumb', {
-    template: '<div class="breadcrumb_container" v-if="attributes.filter.length > 0">' +
-    '<span>{{attributes.attr_id}}&nbsp;&nbsp;:&nbsp;&nbsp;</span><div class="breadcrumd_items">' +
-    '<div v-if="filters.filterType === \'RangedFilter\'"><span class="breadcrumb_item">{{filters[0]}} -- {{filters[1]}}</span>' +
-    '<img class="breadcrumb_remove" src="../../../../images/remove_breadcrumb_icon.png" @click="removeFilter(filters)"></div><div' +
+    template: '<span class="breadcrumb_container" v-if="attributes.filter.length > 0">' +
+    '<span>{{attributes.display_name}}&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="breadcrumb_items">' +
+    '<span v-if="filters.filterType === \'RangedFilter\'"><span class="breadcrumb_item">{{filters[0]}} -- {{filters[1]}}</span>' +
+    '<img class="breadcrumb_remove" src="../../../../images/remove_breadcrumb_icon.png" @click="removeFilter(filters)"></span><span' +
     ' v-else>' +
-    '<div v-for="filter in filters" style="display:inline-block;">' +
+    '<span v-for="filter in filters" style="display:inline-block;">' +
     '<span class="breadcrumb_item">{{filter}}</span>' +
     '<img class="breadcrumb_remove" src="../../../../images/remove_breadcrumb_icon.png" @click="removeFilter(filter)">' +
-    '</div></div></div></div>',
+    '</span></span></span></span>',
     props: [
       'filters', 'attributes'
     ],
