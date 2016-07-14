4143dd491312912ca2ed3a08d7d01a96e1391878
jxu8 <jamesxu@Jamess-MacBook-Pro.local>
2016-07-14 12:45:21 -0400

    initial template

--- app/scripts/views/components/lineChart/lineChartTemplate.js
+++ app/scripts/views/components/lineChart/lineChartTemplate.js
@@ -4,23 +4,28 @@
  * and open the template in the editor.
  */
 'use strict';
-Vue.component('lineChart',{
-        template:'<chart-operations :display-name="displayName"></chart-operations>',
-        props: [],
-        data:{
-            displayName: "Line Chart"
-        },
-        watch:{    
-        },
-        events:{    
-        },
-        methods:{   
-        },
-        ready:function(){   
-        }
-});
-
-var vm= new Vue({
-    el:'#linechart'
-});
-
+(function(Vue, dc, iViz, $){
+    Vue.component('lineChart',{
+            template:'<div class="grid-item grid-item-h-1 grid-item-w-1" ' +
+                     'class="study-view-dc-chart study-view-pie-main">' +
+                     '<chart-operations :has-chart-title="hasChartTitle" :display-name="displayName">' +
+                     '</chart-operations></div>',
+            props: [],
+            data:function(l) {
+                l="Line Chart";
+                return {
+                    displayName: l,
+                    hasChartTitle: true
+                };
+            },
+            watch:{    
+            },
+            events:{    
+            },
+            methods:{   
+            },
+            ready:function(){   
+            }
+    });
+})(window.Vue, window.dc, window.iViz,
+  window.$ || window.jQuery);
