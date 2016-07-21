0192127fb8e8481dc1b6c51a87d05b152414bf97
jxu8 <jamesxu@Jamess-MacBook-Pro.local>
2016-07-21 17:57:15 -0400

    removed return content, fixed rangechartid

--- app/scripts/views/components/lineChart/lineChart.js
+++ app/scripts/views/components/lineChart/lineChart.js
@@ -1,36 +1,64 @@
 'use strict';
 (function(iViz, dc, _, $, d3) {
-    iViz.view.component.lineChart = function(ndx, data){ //must import both ndx (for crossfiltered data) and data (array containing data)
-        var content = {};
+    iViz.view.component.LineChart = function(ndx, data, opts, attributes){ //must import both ndx (for crossfiltered data) and data (array containing data)
+        var attr_id = attributes.attr_id;
+        var content = this;
+        //content.downloadData = {};
+        
         content.init = function initDCLineChart(){
-                    var lineChart = dc.lineChart("#line-chart"); //initialize line chart; random dates created in util.js         
-                         
-                    var parseDate = d3.time.format("%-m-%-d-%Y").parse; //function to parse the date string (with specific format) to a date object
-                    data.forEach(function(d){ //parse every date in the data array
-                        d.DATE_OF_DIAGNOSIS = parseDate(d.DATE_OF_DIAGNOSIS);
-                    }); //each date only has month-date-year; time is removed
-                    
-                    var dateDimension = ndx.dimension(function(d){ //d refers to each object(the element) in the "data" array
-                        return d.DATE_OF_DIAGNOSIS;
-                    });//create date dimension
-                                        
-                    var dateByFrequency = dateDimension.group();//create date group
-                    
-                    lineChart.width(400).height(300)
-                            .dimension(dateDimension)
-                                .group(dateByFrequency)
-                            .transitionDuration(1000)
-                            .margins({top: 30, right: 50, bottom: 25, left: 40})
-                            .mouseZoomable(true)
-                            .elasticY(true)
-                            .x(d3.time.scale().domain(d3.extent(data, function(d) { return d.DATE_OF_DIAGNOSIS; }))) //d3.extent uses the data array 
-                                                                                                                     //to determine the min. and max. date to generate scale
-                            .xAxis();
+            var parseDate = d3.time.format("%-m-%-d-%Y").parse; //function to parse the date string (with specific format) to a date object
+            
+            data.forEach(function(d){ //parse every date in the data array
+                d[attr_id] = parseDate(d[attr_id]); //d[attr_id] = d.DATE_OF_DIAGNOSIS b/c attr_id is a variable
+            }); //each date only has month-date-year; time is removed
+            
+            var dateDimension = ndx.dimension(function(d){ //d refers to each object(the element) in the "data" array
+                return d[attr_id];
+            });//create date dimension
+            
+            var dateByFrequency = dateDimension.group();//create date group
+        
+            var lineChart = dc.lineChart(opts.lineChartTarget, opts.groupid); //initialize line chart; random dates created in util.js
+                                                                              //second parameter adds line chart to the chart group
+            var rangeChart = dc.barChart(opts.rangeChartTarget, opts.groupid);
 
-                    lineChart.render(); //need to render to draw the actual chart
-                };
-        return content;
+            lineChart.width(375).height(230)
+                .dimension(dateDimension)
+                .group(dateByFrequency)
+                .transitionDuration(1000)
+                .margins({top: 10, right: 10, bottom: 25, left: 30})
+                .mouseZoomable(true)
+                .brushOn(false)
+                .elasticY(true)
+                .rangeChart(rangeChart)
+                .x(d3.time.scale().domain(d3.extent(data, function(d) { return d[attr_id]; }))) //d3.extent uses the data array 
+                                                                                                //to determine the min. and max. date to generate scale
+                .round(d3.time.month.round)
+                .xUnits(d3.time.months)
+                .xAxis()
+                .ticks(3);
+                                                                                                //TODO: create line chart functionality where 15 days is combined into one data point on the line chart; the value
+                                                                                                //at that date would be the sum of the values of the individual 15 days
+
+            rangeChart.width(375).height(70) //use range chart to select the range of the line chart
+                .margins({top: 10, right: 10, bottom: 25, left: 30})
+                .dimension(dateDimension)
+                .group(dateByFrequency)
+                .centerBar(true)
+                .gap(10)
+                .x(d3.time.scale().domain(d3.extent(data, function(d) { return d[attr_id]; })))
+                .round(d3.time.day.round)
+                .alwaysUseRounding(true)
+                .xUnits(d3.time.days)
+                .xAxis()
+                .ticks(d3.time.months, 2); //show a tick every 2 months on the range chart      
+//                    lineChart.render(); //no need to render the line chart after adding it to the group of charts (vuecore.js handles rendering)
+        };
     };
+    iViz.view.component.LineChart.prototype = new iViz.view.component.GeneralChart('lineChart'); //prototype of linechart is a new instance of GeneralChart, but with specific functionalities of lineChart
+                                                                                                 //GeneralChart is defined in generalChart.js
+    iViz.view.component.LineChart.constructor = iViz.view.component.LineChart; //constructor is a property that references the function that creates the object iViz..lineChart; assigning iViz...lineChart
+                                                                               //to the constructor ensures that calling it will create a new lineChart each time
 })(window.iViz,window.dc,window._,
 window.$ || window.jQuery, window.d3);
 
--- app/scripts/views/components/lineChart/lineChartTemplate.js
+++ app/scripts/views/components/lineChart/lineChartTemplate.js
@@ -6,29 +6,52 @@
 'use strict';
 (function(Vue, dc, iViz, $){
     Vue.component('lineChart',{
-            template:'<div id = "line-chart" class="grid-item grid-item-h-2 grid-item-w-2" class="study-view-dc-chart">' +
-                     '<chart-operations :has-chart-title="hasChartTitle" :display-name="displayName"> ' +
-                     '</chart-operations></div>', //everything written in template replaces
-                                                                     //the tag in the html file
-            props: ['ndx', 'data'],
+            template:'<div id = "line-chart" class="grid-item grid-item-h-2 grid-item-w-2" class="study-view-dc-chart" @mouseenter="mouseEnter" @mouseleave="mouseLeave">' +
+                     '<chart-operations :has-chart-title="hasChartTitle" :display-name="displayName" :chart-id="chartId" '+ //don't need rangeChartId in chart operations b/c chart operations doesn't handle the range chart
+                     ':attributes="attributes" :show-operations="showOperations"></chart-operations><div id={{chartId}}>' +
+                     '</div><div id={{rangeChartId}} class= "range-chart-class"></div></div>',  //everything written in template replaces
+                                                            //the tag in the html file
+                                                            //create an outer div to hold both the line chart and the range chart
+                                                            //chartId creates element id for download button
+            props: ['ndx', 'data', 'groupid', 'attributes'], //groupid puts line chart in the same group as the other charts(interactive with other graphs)
+                                                             //attribute contains the 
             data:function() {
                 return {
-                    displayName: "Line Chart",
-                    hasChartTitle: true
+                    displayName: this.attributes.display_name,
+                    hasChartTitle: true,
+                    chartId: 'chart-new-' + this.attributes.attr_id.replace(/\(|\)/g, ""),
+                    showOperations: false,
+                    rangeChartId: 'range-chart-new-' + this.attributes.attr_id.replace(/\(|\)/g, "")
+//                    charDivId: 'chart-' + this.attributes.attr_id.replace(/\(|\)/g, "") + '-div'
                 };
             },
             watch:{    
             },
-            events:{    
+            events:{  
+//                'closeChart':function(){
+//                $('#' +this.charDivId).qtip('destroy');
+//                this.$dispatch('close');
+//                }     
             },
-            methods:{   
-            },
+            methods:{
+                mouseEnter: function() {
+                            this.showOperations = true;
+
+                },
+                mouseLeave: function() {
+                              this.showOperations = false;
+                            }
+                },
             ready:function(){
-//                console.log(this.ndx);
-//                console.log(iViz.view.component.lineChart(this.ndx).init());
-               new iViz.view.component.lineChart(this.ndx, this.data).init(); //create new instance of a line chart
+                var opts = {
+                 groupid:this.groupid,
+                 lineChartTarget: "#" + this.chartId,
+                 rangeChartTarget: "#" + this.rangeChartId
+                }; 
+                
+                new iViz.view.component.LineChart(this.ndx, this.data, opts, this.attributes).init(); //create new instance of a line chart
                                                                         //each time this function is called - will not 
-                                                                        //reference the same one everytime                                                                                                                                                                                                                                      
+                                                                      //reference the same one everytime                                                                                                                                                                                                                                      
             }
     });
 })(window.Vue, window.dc, window.iViz,
