1f8dea98d44e6e31879f35d311cd2b39e8d35cd5
jxu8 <jamesxu@Jamess-MacBook-Pro.local>
2016-07-22 17:15:47 -0400

    added range chart and other line chart functionalities

--- app/scripts/views/components/lineChart/lineChartTemplate.js
+++ app/scripts/views/components/lineChart/lineChartTemplate.js
@@ -7,9 +7,11 @@
 (function(Vue, dc, iViz, $){
     Vue.component('lineChart',{
             template:'<div id = "line-chart" class="grid-item grid-item-h-2 grid-item-w-2" class="study-view-dc-chart">' +
-                     '<chart-operations :has-chart-title="hasChartTitle" :display-name="displayName" :groupid="groupid"> ' +
-                     '</chart-operations></div>', //everything written in template replaces
-                                                                     //the tag in the html file
+                     '<chart-operations :has-chart-title="hasChartTitle" :display-name="displayName"> ' +
+                     '</chart-operations><div id ="the-line-chart"></div>' +
+                     '<div id="range-chart"></div></div>',  //everything written in template replaces
+                                                            //the tag in the html file
+                                                            //create an outer div to hold both the line chart and the range chart
             props: ['ndx', 'data', 'groupid'], //groupid puts line chart in the same group as the other charts(interactive with other graphs)
             data:function() {
                 return {
