c31f7597354db0f069c068afa41d789cbdc7e799
Karthik Kalletla <kalletlakarthik@gmail.com>
2016-11-14 10:37:18 -0500

    Update session module library

--- app/_components/vc-session.js
+++ app/_components/vc-session.js
@@ -1,4 +1,8 @@
 'use strict';
+// Move vcSession initialization to here since the sessionEvent.js
+// is the first one to be called in the dependency list.
+window.vcSession = window.vcSession ? window.vcSession : {};
+
 (function(vcSession, _) {
   if (!_.isObject(vcSession)) {
     vcSession = {};
