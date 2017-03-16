205791d6996a798eb9c2bc22f56aa69ce70f7fa6
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-03-16 15:06:39 -0400

    Move Saved/Failed message into same dialog of saving cohort

--- app/scripts/views/components/vc/sessionComponent.js
+++ app/scripts/views/components/vc/sessionComponent.js
@@ -1,8 +1,8 @@
 'use strict';
 (function(Vue, $, vcSession) {
   Vue.component('sessionComponent', {
-    template: '<div v-if="showManageButton || showSaveButton" class="input-group">' +
-    '<span class="input-group-addon">Cohort</span>' +
+    template: '<div v-if="showManageButton || showSaveButton" ' +
+    'class="input-group"><span class="input-group-addon">Cohort</span>' +
     '<div class="input-group-btn">' +
     '<button v-if="showSaveButton" type="button" ' +
     'class="btn btn-default save-cohort-btn">' +
@@ -85,7 +85,7 @@
             'iviz-save-cohort-btn-qtip'
           },
           show: {event: 'click', ready: false},
-          hide: {fixed: true, delay: 1000, event: 'mouseleave'},
+          hide: false,
           position: {
             my: 'top center',
             at: 'bottom center',
@@ -93,11 +93,17 @@
           },
           events: {
             render: function(event, api) {
-              $('.iviz-save-cohort-btn-qtip .save-cohort').click(function() {
-                var cohortName =
-                  $('.iviz-save-cohort-btn-qtip .cohort-name').val();
+              var tooltip = $('.iviz-save-cohort-btn-qtip .qtip-content');
+              tooltip.find('.save-cohort').click(function() {
+                tooltip.find('.saving').css('display', 'block');
+                tooltip.find('.close-dialog').css('display', 'none');
+                tooltip.find('.saved').css('display', 'none');
+                tooltip.find('.dialog').css('display', 'none');
+                api.reposition();
+
+                var cohortName = tooltip.find('.cohort-name').val();
                 var cohortDescription =
-                  $('.iviz-save-cohort-btn-qtip .cohort-description').val();
+                  tooltip.find('.cohort-description').val();
                 if (_.isObject(vcSession)) {
                   self_.updateStats = true;
                   self_.$nextTick(function() {
@@ -105,39 +111,55 @@
                       self_.selectedPatientsNum, self_.selectedSamplesNum,
                       self_.userid, cohortName, cohortDescription || '')
                       .done(function() {
-                        $.notify('Added to new Virtual Study', {
-                          type: 'success'
-                        });
+                        tooltip.find('.savedMessage').text(
+                          'Added to new Virtual Study');
                       })
                       .fail(function() {
-                        $.notify('Failed to connect to Portal service, ' +
-                          'but we would be able to keep it within the browser.', {
-                          type: 'warning'
-                        });
+                        tooltip.find('.savedMessage').html(
+                          '<i class="fa fa-exclamation-triangle"></i>' +
+                          'Failed to save virtual cohort, ' +
+                          'please try again later.');
                       })
                       .always(function() {
-                        api.hide();
-                        $('.iviz-save-cohort-btn-qtip .cohort-name').val('');
-                        $('.iviz-save-cohort-btn-qtip .cohort-description').val('');
-                        $('.iviz-save-cohort-btn-qtip .save-cohort')
+                        tooltip.find('.close-dialog')
+                          .css('display', 'inline-block');
+                        tooltip.find('.saved').css('display', 'block');
+                        tooltip.find('.saving').css('display', 'none');
+                        tooltip.find('.dialog').css('display', 'none');
+                        tooltip.find('.cohort-name').val('');
+                        tooltip.find('.cohort-description').val('');
+                        tooltip.find('.save-cohort')
                           .attr('disabled', true);
+                        api.reposition();
                       });
                   });
                 }
               });
-              $('.iviz-save-cohort-btn-qtip .cohort-name')
+              tooltip.find('.close-dialog i').click(function() {
+                api.hide();
+              });
+              tooltip.find('.cohort-name')
                 .keyup(function() {
-                  if ($('.iviz-save-cohort-btn-qtip .cohort-name').val() === '') {
-                    $('.iviz-save-cohort-btn-qtip .save-cohort')
+                  if (tooltip.find('.cohort-name').val() === '') {
+                    tooltip.find('.save-cohort')
                       .attr('disabled', true);
                   } else {
-                    $('.iviz-save-cohort-btn-qtip .save-cohort')
+                    tooltip.find('.save-cohort')
                       .attr('disabled', false);
                   }
                 });
+            },
+            show: function() {
+              var tooltip = $('.iviz-save-cohort-btn-qtip .qtip-content');
+              tooltip.find('.close-dialog').css('display', 'inline-block');
+              tooltip.find('.dialog').css('display', 'block');
+              tooltip.find('.saving').css('display', 'none');
+              tooltip.find('.saved').css('display', 'none');
             }
           },
-          content: '<div><div class="input-group">' +
+          content: '<div><div class="close-dialog">' +
+          '<i class="fa fa-times-circle-o"></i></div>' +
+          '<div class="dialog"><div class="input-group">' +
           '<input type="text" class="form-control cohort-name" ' +
           'placeholder="New Cohort Name"> <span class="input-group-btn">' +
           '<button class="btn btn-default save-cohort" ' +
@@ -145,7 +167,14 @@
           '</div><div>' +
           '<textarea class="form-control cohort-description" rows="5" ' +
           'placeholder="New Cohort Description (Optional)"></textarea>' +
-          '</div></div>'
+          '</div></div>' +
+          '<div class="saving" style="display: none;">' +
+          '<i class="fa fa-spinner fa-spin"></i> Saving virtual cohort</div>' +
+          '<div class="saved" style="display: none;">' +
+          '<span class="savedMessage"></span>' +
+          '<button class="btn btn-default btn-sm query"' +
+          '>Query Virtual Study</button></div>' +
+          '</div>'
         });
       }
     }
