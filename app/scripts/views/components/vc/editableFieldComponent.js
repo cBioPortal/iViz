b179f2b1e6d40c9f60d8a95edbdd06e5928b4068
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-03-17 14:15:35 -0400

    Add minor style change

--- app/scripts/views/components/vc/editableFieldComponent.js
+++ app/scripts/views/components/vc/editableFieldComponent.js
@@ -7,6 +7,7 @@
     ' type="text" v-model="name" placeholder="My Virtual' +
     ' Study"/></div><div v-if="type==\'textarea\'"><textarea rows="4"' +
     ' cols="50" v-model="name"></textarea></div></div><div' +
+    ' style="white-space: pre-line !important;"' +
     ' v-else="edit"><span>{{ name }}</span></div>'
   });
 })(window.Vue);
