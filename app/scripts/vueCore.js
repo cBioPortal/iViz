b0bfa9b9c7c2b15acd96b17d340132a313e60b82
Jing Su <victoriasu1994@gmail.com>
2017-08-01 09:18:03 -0400

    Make scatter async (#537)

--- app/scripts/vueCore.js
+++ app/scripts/vueCore.js
@@ -167,6 +167,7 @@
               self_.$nextTick(function() {
                 if (_attrAdded) {
                   $.when(iViz.updateGroupNdx(attrData.group_id, attrData.attr_id)).then(function(isGroupNdxDataUpdated) {
+                    attrData.addChartBy = 'user';
                     self_.groups[_groupIdToPush].attributes.push(attrData);
                     if (isGroupNdxDataUpdated) {
                       self_.$broadcast('add-chart-to-group', attrData.group_id);
@@ -246,8 +247,6 @@
                   selectedCaseUIDs = selectedCaseUIDs.concat(caseUIDs);
                 }
               });
-
-
 
               if (unmappedCaseIDs.length > 0) {
                 new Notification().createNotification(selectedCaseUIDs.length +
