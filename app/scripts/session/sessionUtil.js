24723bf38388484f6a6833372e21e8cc8bb85662
Hongxin Zhang <hongxin@cbio.mskcc.org>
2017-11-20 17:31:37 -0500

    Fix typo

--- app/scripts/session/sessionUtil.js
+++ app/scripts/session/sessionUtil.js
@@ -50,7 +50,7 @@
       if (name) {
         _virtualCohort.name = name;
       } else {
-        _virtualCohort.name = cases.length > 1 ? "Combined Studies" : "Selected Study";
+        _virtualCohort.name = cases.length > 1 ? "Combined Study" : "Selected Study";
       }
       _virtualCohort.description = description || '';
       def.resolve(_virtualCohort);
