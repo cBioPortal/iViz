54259d1c9ce9f96dde8a96609d9a5d05dc2ae271
Hongxin Zhang <jackson.zhang.828@gmail.com>
2018-01-04 12:37:50 -0500

    Pre-fill the current selected sample/patient ids in  custom case selection box
    After clicking on case type radio, the list will be updated accordingly

--- app/scripts/views/components/customPlots/CaseDialogBox.js
+++ app/scripts/views/components/customPlots/CaseDialogBox.js
@@ -2,7 +2,7 @@
  * Created by kalletlak on 7/8/16.
  */
 'use strict';
-(function(Vue, dc, iViz, $) {
+(function(Vue, dc, iViz, _, $) {
   var headerCaseSelectCustomDialog = {
     // Since we're only creating one modal, give it an ID so we can style it
     id: 'iviz-case-select-custom-dialog',
@@ -29,17 +29,25 @@
     style: 'qtip-light qtip-rounded qtip-wide'
   };
   Vue.component('customCaseInput', {
-    template: '<input type="button" id="custom-case-input-button" ' +
+    template: '<div style="display: inline-flex"><input type="button" id="custom-case-input-button" ' +
     'class="iviz-header-button" value="Select cases by IDs"/>' +
     '<div class="iviz-hidden" id="iviz-case-select-custom-dialog">' +
     '<b>Please input IDs (one per line)</b></br><textarea rows="20" cols="50" ' +
     'id="iviz-case-select-custom-input" v-model="casesIdsList"></textarea>' +
     '<br/><label><input type="radio" v-model="caseSelection" ' +
-    'value="sample" checked>By sample ID</label><label><input type="radio" ' +
-    'v-model="caseSelection" value="patient">' +
-    'By patient ID</label><button type="button" @click="SetCasesSelection()" ' +
-    'style="float: right;">Select</button></div>',
-    props: [],
+    'value="sample" checked @click="updateCaseIds(\'sample\')">By sample ID</label><label><input type="radio" ' +
+    'v-model="caseSelection" value="patient" @click="updateCaseIds(\'patient\')">' +
+    'By patient ID</label><button type="button" @click="setCasesSelection()" ' +
+    'style="float: right;">Select</button></div></div>',
+    props: {
+      stats: {
+        type: Object
+      },
+      updateStats: {
+        type: Boolean,
+        default: false
+      }
+    },
     data: function() {
       return {
         caseSelection: '',
@@ -48,15 +56,35 @@
     },
     events: {},
     methods: {
-      SetCasesSelection: function() {
+      setCasesSelection: function() {
         var caseIds = this.casesIdsList.trim().split(/\n/);
         this.$dispatch('set-selected-cases', this.caseSelection, _.uniq(caseIds));
+      },
+      updateCaseIds: function(type) {
+        var cases = [];
+        _.each(this.stats.studies, function(t) {
+          var targetGroup = type === 'patient' ? t.patients : t.samples;
+          _.each(targetGroup, function(caseId) {
+            cases.push(t.id + ':' + caseId);
+          });
+        });
+        this.casesIdsList = cases.join('\n');
       }
     },
     ready: function() {
+      var self_ = this;
       var _customDialogQtip =
         $.extend(true, {}, headerCaseSelectCustomDialog);
       _customDialogQtip.position.target = $(window);
+      _customDialogQtip.events = {
+        show: function() {
+          var tooltip = $('.iviz-save-cohort-btn-qtip .qtip-content');
+          self_.updateStats = true;
+          self_.$nextTick(function() {
+            self_.updateCaseIds('sample');
+          });
+        }
+      };
       _customDialogQtip.content.text = $('#iviz-case-select-custom-dialog');
       $('#custom-case-input-button').qtip(_customDialogQtip);
     }
@@ -65,5 +93,6 @@
   window.Vue,
   window.dc,
   window.iViz,
+  window._,
   window.$ || window.jQuery
 );
